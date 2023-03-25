bd12abdb253dcc0c07ffd25bb0ee185707e09cf8
Kristijonas <kekristis0@gmail.com>
2023-03-25 15:57:38 +0200

    Working Lithuanian language decoder

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
-import json
+import pandas as pd
+import re
 from datetime import datetime
+import json
 
-f = open('message_1.json')
+def decoder(string):
+  patterns = {
+    r'\u00c4\u0085': 'ą',
+    r'\u00c4\u008d': 'č',
+    r'\u00c4\u0099': 'ę',
+    r'\u00c4\u0097': 'ė',
+    r'\u00c4\u00af': 'į',
+    r'\u00c5\u00a1': 'š',
+    r'\u00c5\u00b3': 'ų',
+    r'\u00c5\u00ab': 'ū',
+    r'\u00c5\u00be': 'ž',
+  }
+  for pattern, replacement in patterns.items():
+      string = re.sub(pattern, replacement, string)
+  return string
 
-data = json.load(f)
+def json_df(data):
+    messages_list = data['messages']
 
+    df = pd.DataFrame(messages_list)
+    df.rename(columns={'timestamp_ms': 'time', 'sender_name': 'name'}, inplace=True)
+    df['content'].fillna('', inplace=True)
+    df['time'] = df['time'].apply(lambda x: datetime.fromtimestamp(x / 1000).strftime("%Y-%m-%d %a %H:%M:%S"))
+    df['time'] = pd.to_datetime(df['time'])
 
-for message in data["messages"]:
-    try:
-        date = datetime.fromtimestamp(message["timestamp_ms"] / 1000).strftime("%Y-%m-%d %a %H:%M:%S")
-        sender = message["sender_name"]
-        content = message["content"]
-        print(date, sender)
-    except KeyError:
-        pass
+    df['name'] = df['name'].apply(lambda x: decoder(x))
+    df['content'] = df['content'].apply(lambda x: decoder(x))
+    return df
 
+with open('message_1.json') as json_file:
+    data = json.load(json_file)
 
-f.close()
+df = json_df(data)
+
+df_you = df[df['name'] == 'Your name']
+df_other = df[df['name'] == 'Other name']
+
+msgr = pd.DataFrame({'name': [df_other['name'][0]], 'sent': [len(df_other)], 'got': [len(df_you)]})
+msgr = msgr.set_index('name')
+
+print(msgr)
